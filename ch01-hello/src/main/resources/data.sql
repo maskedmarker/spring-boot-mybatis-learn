81322049965787ff829a10f22e7d6ff8cb4a3558
maskedmarker <changjianghui0822@gmail.com>
2025-03-15 20:20:07 +0800

    no msg

--- ch01-hello/src/main/resources/data.sql
+++ ch01-hello/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into t_user (name, email) values ('001', '001@qq.com');
-insert into t_user (name, email) values ('002', '002@qq.com');
-insert into t_user (name, email) values ('003', '003@qq.com');
-insert into t_user (name, email) values ('004', '004@qq.com');
-insert into t_user (name, email) values ('005', '005@qq.com');
+insert into t_user (name, email, user_status) values ('001', '001@qq.com', '0');
+insert into t_user (name, email, user_status) values ('002', '002@qq.com', '1');
+insert into t_user (name, email, user_status) values ('003', '003@qq.com', '1');
+insert into t_user (name, email, user_status) values ('004', '004@qq.com', '1');
+insert into t_user (name, email, user_status) values ('005', '005@qq.com', '2');
